afde760f69b55c9569a20df857f1cae96034916d
December81 <megastudio@outlook.fr>
2023-04-16 22:46:50 +0200

    Une application React intégrée à Drupal pour afficher une liste d'articles et leurs commentaires récupérés via l'API JSON de Drupal.

---
+++ web/themes/mon_theme_react/react-app/precache-manifest.89333fcf94e66602fe70c833a77af31e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab48b8dd2c46739030c9",
+    "url": "/static/css/main.22834ec7.chunk.css"
+  },
+  {
+    "revision": "ab48b8dd2c46739030c9",
+    "url": "/static/js/main.ab48b8dd.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "9ef6c58dca8dff32e2dd",
+    "url": "/static/js/2.9ef6c58d.chunk.js"
+  },
+  {
+    "revision": "a3b6dd91a2de68d25800",
+    "url": "/static/js/runtime~main.a3b6dd91.js"
+  },
+  {
+    "revision": "4d93f8fa380353e85a0f52640e832e94",
+    "url": "/index.html"
+  }
+];
